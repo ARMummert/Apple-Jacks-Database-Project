c55525aeca4f68eba29c3cb39902c44638b24cd5
Amy Mummert <mummeram@flip3.engr.oregonstate.edu>
2022-11-09 15:09:07 -0800

    updates

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 
 var express = require('express');   
 var app     = express();    
-PORT = 4418;
+PORT = 4008;
 var os = require('os');
+const { promisify } = require('promisify');
+
+
+// Express Handlebars
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -13,11 +23,29 @@ app.use(express.urlencoded({extended: true}));
 // Database
 var db = require('./database/db-connector')
 
-// Routes
+// Routes - Homepage
 
-app.get('/', function (req, res) {
-  res.send('Hi, this is our main page!')
+app.get('/', function(req, res)
+    {
+        res.render('/index');                    
+    });     
+
+// Competitions
+
+app.get('/competitions', function (req, res){
+  //Search
+  let competitions;
+  if (req.query.compeitionName === undefined) {
+    competitions = 'SELECT * FROM Competitions;';
+  } else {
+    competitions = `SELECT * FROM Competitions WHERE competitionName LIKE '${req.query.compeitionName}%';`;
+  }
+
+  db.pool.query(competitions, function(error, rows, fields) {
+    return res.render('competitions', {data: rows});
+  });
 });
+
 
 //Exceptions Handling
 

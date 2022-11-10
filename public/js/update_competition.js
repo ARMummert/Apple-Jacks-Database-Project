29b04ff4bd5c0543be9dbe8671e156ed1fe28799
Joel  Vrieze <97073281+jdubvrieze@users.noreply.github.com>
2022-11-10 15:39:15 -0800

    Update update_competition.js

---
+++ public/js/update_competition.js
@@ -0,0 +1,7 @@
+// Get the objects we need to modify
+let updateCompetitionForm = document.getElementById('update-competition-form');
+
+// Modify the objects we need
+updateCompetitionForm.addEventListener("submit", function (e) {
+   
+    

a585750cab9907feb4ffcbadf487d41dee5c8270
Amy Mummert <mummeram@flip1.engr.oregonstate.edu>
2022-11-11 12:01:04 -0800

    update

--- app.js
+++ app.js
@@ -166,22 +166,6 @@ app.get('/project-development', function(req, res){
   res.render('project-development')
 });
 
-
-//Exceptions Handling
-
-app.use(function(req,res){
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
-});
-
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.send('500 - Server Error');
-});
-
 // LISTENER
 
 app.listen(PORT, function () {

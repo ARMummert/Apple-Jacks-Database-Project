0b591b02e1cb38facff44f0fdbc1ed09c479245a
Joel  Vrieze <97073281+jdubvrieze@users.noreply.github.com>
2022-11-12 08:31:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.get('/', function(req, res)
   {
-    res.render('main', {layout : './index'});                  
+    res.render('index');                  
   });   
 
 // Competitions

e786871b6c4ac9e73f3aa252a2732e3f9ad0c009
Amy Mummert <mummeram@flip1.engr.oregonstate.edu>
2022-11-17 13:10:13 -0800

    events.hbs complete for now, competitions.hbs - took out backslash, add_events - complete for now, app.js - added crud for events

--- app.js
+++ app.js
@@ -29,24 +29,24 @@ app.get('/', function(req, res)
 // Routes - Competitions
 
 app.get('/competitions', function (req, res){
-let query1;
-if (req.query.competitionName === undefined)
-{
-  query1 = `SELECT competitionID as 'ID', competitionName as 'Competition Name', Date, startTime as 'Start Time', locationName as 'Location Name',
-  locationAddress as 'Location Address',locationPhone as 'Location Phone' FROM Competitions;`;
-}
-else 
-{
-  query1 = `SELECT competitionID as 'ID', competitionName as 'Competition Name', Date, startTime as 'Start Time', locationName as 'Location Name',
-  locationAddress as 'Location Address',locationPhone as 'Location Phone' FROM Competitions
-  WHERE competitionName = :Search_input_field;`;
-   }    
-
-db.pool.query(query1, function(error, rows, fields) {
+  let competitions;
+  if (req.query.competitionName === undefined)
+  {
+    competitions = `SELECT competitionID as ID, competitionName as 'Competition', Date, startTime as 'Time', locationName as 'Location',
+    locationAddress as 'Address',locationPhone as 'Phone' FROM Competitions;`;
+  }
+  else 
+  {
+    competitions = `SELECT competitionID as 'ID', competitionName as 'Competition', Date, startTime as 'Time', locationName as 'Location',
+    locationAddress as 'Address',locationPhone as 'Phone' FROM Competitions
+    Where competitionName LIKE "${req.query.competitionName}%";`;
+  }
   
-  return res.render('competitions', {data: rows});
-});
-});
+  db.pool.query(competitions, function(error, rows, fields) {
+    return res.render('competitions', {data: rows});
+  });
+  });
+  
 
 // Create Competitions
 app.post('/add-competition-ajax', function(req, res) {
@@ -105,28 +105,20 @@ app.put('/put-competition-ajax', function(req, res, next) {
   let updateCompetition = `UPDATE Competitions SET competitionName = 
    '${data.competitionName}', date = '${data.date}', startTime = '${data.startTime}', locationName = '${data.locationName}', locationAddress = '${data.locationAddress}', locationPhone = '${data.locationPhone}' WHERE competitionID = ?;`
   
-  let selectCompetition = `SELECT * FROM Competitons WHERE competitionID = ?`
+  let selectCompetition = `SELECT competitionID, competitionName, data, startTime, locationName, locationAddress, locationPhone FROM Competitons WHERE competitionID = ?`
     db.pool.query(
       updateCompetition,
 
-      [
-        data['competitionName'],
-        data['date'],
-        data['startTime'],
-        data['locationName'],
-        data['locationAddress'],
-        data['locationPhone'],
-        competitionID
-      ],
-//     [
-//        competitionID,
- //       competitionName,
- //       date,
-  //      startTime,
-    //    locationName,
-      //  locationAddress,
-      //  locationPhone
- //     ],
+        [
+        competitionID,
+        competitionName,
+        date,
+        startTime,
+        locationName,
+        locationAddress,
+        locationPhone
+       ],
+
     function (error, rows, fields) {
       if (error) {
         console.log(error);
@@ -171,8 +163,8 @@ app.get('/events', function (req, res){
   let query1;
   if (req.query.eventName === undefined)
   {
-    query1 = `SELECT eventID as 'ID',eventName as 'Event Name', Competitions.competitionName as Competition,
-    Divisions.divisionName as Divsion, EventLevels.eventlevelName as 'Event Level'
+    query1 = `SELECT eventID as 'ID',eventName as 'Event', Competitions.competitionName as 'Competition',
+    Divisions.divisionName as 'Divsion', EventLevels.eventlevelName as 'Event-Level'
     FROM Events
     INNER JOIN Competitions ON Events.competitionID = Competitions.competitionID
     INNER JOIN Divisions ON Events.divisionID = Divisions.divisionID
@@ -180,8 +172,8 @@ app.get('/events', function (req, res){
   }
   else 
   {
-    query1 = `SELECT eventID as 'ID',eventName as 'Event Name', Competitions.competitionName as Competition,
-    Divisions.divisionName as Divsion, EventLevels.eventlevelName as 'Event Level'
+    query1 = `SELECT eventID as 'ID',eventName as 'Event', Competitions.competitionName as 'Competition',
+    Divisions.divisionName as 'Divsion', EventLevels.eventlevelName as 'Event-Level'
     FROM Events
     INNER JOIN Competitions ON Events.competitionID = Competitions.competitionID
     INNER JOIN Divisions ON Events.divisionID = Divisions.divisionID
@@ -194,9 +186,102 @@ app.get('/events', function (req, res){
   });
   });
 
+  // Create Events
+app.post('/add-event-ajax', function(req, res) {
+  let data = req.body;
+  
+  // Create Events Query
+ 
+    query1 = `INSERT INTO Events(competitionID,divisionID,eventlevelID,eventName)
+    VALUES (
+      '${data.competitionID}',
+      '${data.divisionID}',
+      '${data.eventlevelID}',
+      '${data.eventName}',
+      `;
+    
+    
+  db.pool.query(query1, function(error, rows, fields) {
+    if (error) {
+      console.log(error)
+      res.sendStatus(400);
+    }
+    else
+    {
+      res.send(rows);
+                    
+    }
+    })
+  });    
+ 
+  // Update Event
+app.put('/put-event-ajax', function(req, res, next) {
+  let data = req.body;
+  let eventID = parseInt(data.id);
+  let competitionID = data.competitionIDValue;
+  let divisionID = data.divisionIDValue;
+  let eventlevelID = data.eventlevelIDValue;
+  let eventName = data.eventNameValue;
+  
 
+  let updateEvent = `UPDATE Events SET competitionID = 
+   '${data.competitionID}', divisionID = '${data.divisionID}', eventlevelID = '${data.eventlevelID}', eventName = '${data.eventName}' WHERE eventID = ?;`
+  
+  let selectEvent = `SELECT EventID,competitionID,DivisionID,eventlevelID,eventName from Events
+  WHERE eventID =  ?`
+    db.pool.query(
+      updateEvent,
+
+      [
+        eventID,
+        competitionID,
+        divisionID,
+        eventlevelID,
+        eventName
+        
+      ],
+
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+        res.sendStatus(400);
+      }
+      else {
+      db.pool.query(selectEvent, [eventID], function(error, rows, fields){
+        if (error) {
+          console.log(error)
+          res.sendStatus(400);
+        }
+        else {
+          res.send(rows);
+          res.redirect('/events')
+        }
+      })
+      
+    }
+  })
+});
+
+// Delete Event
+app.delete('/delete-event/', function(req, res, next) {
+  let data = req.body;
+  let eventID = parseInt(data.id);
+  let deleteEvents = 'DELETE FROM Events WHERE Events.eventID = ?';
+
+  db.pool.query(deleteEvents, [eventID], function(error, rows, fields) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(400);
+    }
+    else {
+      res.sendStatus(204);
+      
+    }
+  });
+});
+    
 // LISTENER
 
 app.listen(PORT, function () {
-  console.log('Express started on http://flip3.engr.oregonstate.edu:' + PORT + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://flip1.engr.oregonstate.edu:' + PORT + '; press Ctrl-C to terminate.');
 });

d34f636d7dce120fae7aa973e8ce8e76debb39fc
Amy Mummert <mummeram@flip3.engr.oregonstate.edu>
2022-11-28 12:53:05 -0800

    new athletes_events and athletes add and delete

---
+++ public/js/delete_athlete.js
@@ -0,0 +1,39 @@
+function deleteAthlete(athleteID) {
+    let link = '/delete-athlete/'
+    let data = {
+        id: athleteID
+    };
+    $.ajax({
+        url: link,
+        type:'DELETE',
+        data:JSON.stringify(data),
+        contentType: "application/json; charset=utf=8",
+        success: function(result){
+            deleteRow(athleteID);
+        }
+    });
+}
+
+function deleteRow(athleteID){
+
+    let table = document.getElementById("athletes-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (row.getAttribute("data-value") == athleteID) {
+            table.deleteRow(i);
+            table.deleteDropDown(i);
+            break;
+       }
+    }
+}
+
+
+function deleteDropDown(athleteID) {
+    let selectAthlete = document.getElementById("select-athlete")
+    for (let i = 0; i < selectAthletes.length; i++) {
+        if(Number(selectAthlete.options[i].value) === Number(athleteID)) {
+            selectAthlete[i].remove();
+        }
+    }
+}
